32ab0facd3cab8d7b67e2a0694610c17c6eadb10
Raffael Fidhera <raffaeljkt@gmail.com>
2024-07-09 23:24:09 +0700

    Add files via upload

---
+++ spamwa.py
@@ -0,0 +1,16 @@
+import pyautogui as pt
+import time
+
+limit = input("Enter limit:") # Input Jumlah Pesan
+message = input("Enter message:") # Input Kata Yang Ingin Dikirim
+i = 0
+time.sleep(5)
+
+while i < int(limit):
+    pt.typewrite(message)
+    # the messages are written when - 
+    # the cursor is placed  
+
+    pt.press("enter")
+
+    i+=1
