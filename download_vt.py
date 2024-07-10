0dbb5d226110d0236ae268f180a0c3e19a322db9
Raffael Fidhera <raffaeljkt@gmail.com>
2024-07-10 22:12:56 +0700

    Update download_vt.py

--- download_vt.py
+++ download_vt.py
@@ -1,4 +1,4 @@
-# Program mendownload Vidio Tiktok (VT) dengan menginput linkkkkkkkkkk
+# Program mendownload Vidio Tiktok (VT) dengan menginput linkkkkkkkkk
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
